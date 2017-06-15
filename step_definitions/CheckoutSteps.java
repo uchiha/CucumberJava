af063edc2ee9f6234417c9cca7aa0f0c50a0209c
clar17 <clarenciotan@gmail.com>
2017-06-15 12:58:54 +0800

    first.feature added new feature to this file checkout.java modified implementation details to make use of Lists due to unfixed number of items for checkout checkoutsteps.java major change is to put initialization of Checkout object within the class and not local to any method. Fixed issue of multiple checkout.

--- step_definitions/CheckoutSteps.java
+++ step_definitions/CheckoutSteps.java
@@ -8,19 +8,29 @@ import static org.junit.Assert.*;
 public class CheckoutSteps
 {
 	int bananaPrice = 0;
-	Checkout checkout;
+	int applePrice = 0;
+	Checkout checkout = new Checkout();
 	
 	@Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
 	public void thePriceOfAIsC(String name, int price) throws Throwable
 	{
-		bananaPrice = price;
+		if(name.startsWith("banana"))
+		{
+			bananaPrice = price;	
+		}
+		else
+			applePrice = price;
 	}
 
 	@When("^I checkout (\\d+) \"([^\"]*)\"$")
-	public void iCheckout(int itemCount, String itemName) throws Throwable 
+	public void iCheckout(int itemCount, String fruitName) throws Throwable 
 	{
-		checkout = new Checkout();
-		checkout.add(itemCount, bananaPrice);
+		if(fruitName.startsWith("banana"))
+		{
+			checkout.add(itemCount, bananaPrice, fruitName);
+		}
+		else
+			checkout.add(itemCount, applePrice, fruitName);
 	}
 
 	@Then("^the total price should be (\\d+)c$")
